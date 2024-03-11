83f7dd47b534fbc7723df86e1b4fb83f1efc5e55
johnchuma <johnvchuma@gmail.com>
2024-03-11 09:53:54 +0300

    save changes

--- src/app/(groupDashboard)/(pledgePayouts)/pledgePayouts/[uuid]/page.js
+++ src/app/(groupDashboard)/(pledgePayouts)/pledgePayouts/[uuid]/page.js
@@ -63,14 +63,7 @@ const Page = ({params}) => {
                      return <tr className="" key={key}>
                      <td className="py-2 text-sm">{formatDate(item.createdAt)}</td>
                      <td className="py-2 text-sm">{item.amount}TSH</td>
-                     <td  className=" text-indigo-600 bg-opacity-20  font-bold">
-                         <Link href={`/editIncomeCollections/${item.id}`}>
-                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
-                          className="w-5 h-5 text-green-600">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                            </svg>
-                         </Link>
-                     </td>
+                    
                      <td  className=" text-danger bg-opacity-20 font-bold">
                          <div className="cursor-pointer" onClick={()=>{
                             editIncomeCollection(collection.id,{payouts:[...collection.payouts.filter((e)=>e.id!=item.id)]}).then((data)=>{
